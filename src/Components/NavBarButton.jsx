c16e9af379e2b8e2cf23f43c77945daf0f0842d8
Guleb23 <112973753+Guleb23@users.noreply.github.com>
2025-03-30 20:56:57 +0300

    wwww

--- src/Components/NavBarButton.jsx
+++ src/Components/NavBarButton.jsx
@@ -10,7 +10,7 @@ const NavBarButton = ({ title, to, icon }) => {
             <p className={`hidden md:block  ${location.pathname == to ? "font-bold" : ""}`} >
                 {title}
             </p>
-            <div className={`md:hidden py-2 w-10 ${location.pathname == to ? "bg-[#C1C8CF]" : ""} rounded-xl flex items-center justify-center`}>
+            <div className={`md:hidden py-2 w-10 ${location.pathname == to || location.pathname.startsWith('/documents') ? "bg-[#C1C8CF]" : ""} rounded-xl flex items-center justify-center`}>
                 <picture>
                     <img src={icon} fetchPriority="high" loading='lazy' width={25} height={25} />
                 </picture>
