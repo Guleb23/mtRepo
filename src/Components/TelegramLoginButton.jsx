19aff1c266b4335ee2d74528fbd59ffe5d2b563c
Guleb23 <112973753+Guleb23@users.noreply.github.com>
2025-04-02 03:28:56 +0300

    useRef

--- src/Components/TelegramLoginButton.jsx
+++ src/Components/TelegramLoginButton.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import '../TelegramAuthButton.css'; // Стили для кнопки
 
 const TelegramAuthButton = ({ botName, onAuth }) => {
