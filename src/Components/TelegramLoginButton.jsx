11d3a0e3b36ed3a628cca53fa72b3492a887631b
Guleb23 <112973753+Guleb23@users.noreply.github.com>
2025-04-03 01:33:10 +0300

    фывфыв

--- src/Components/TelegramLoginButton.jsx
+++ src/Components/TelegramLoginButton.jsx
@@ -18,7 +18,6 @@ const TelegramLoginButton = ({
         script.setAttribute('data-telegram-login', botName);
         script.setAttribute('data-size', buttonSize);
         script.setAttribute('data-onauth', 'onTelegramAuth(user)');
-        script.setAttribute('data-request-access', 'write');
         // Включаем чекбокс разрешения сообщений
         script.setAttribute('data-bot-access', 'true');
         // Добавляем скрипт в DOM
