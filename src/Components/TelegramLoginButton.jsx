53453522b8ae196f15bce70fb4224c8a63a772ac
Guleb23 <112973753+Guleb23@users.noreply.github.com>
2025-04-02 19:52:14 +0300

    updaeBot

--- src/Components/TelegramLoginButton.jsx
+++ src/Components/TelegramLoginButton.jsx
@@ -10,7 +10,6 @@ const TelegramLoginButton = ({
     useEffect(() => {
         // Удаляем предыдущий обработчик, если был
         window.onTelegramAuth = undefined;
-
         // Создаем скрипт для виджета Telegram
         const script = document.createElement('script');
         script.src = 'https://telegram.org/js/telegram-widget.js?22';
@@ -18,7 +17,7 @@ const TelegramLoginButton = ({
         script.setAttribute('data-telegram-login', botName);
         script.setAttribute('data-size', buttonSize);
         script.setAttribute('data-onauth', 'onTelegramAuth(user)');
-        script.setAttribute('data-request-access', requestAccess);
+        script.setAttribute('data-request-access', "write");
         // Включаем чекбокс разрешения сообщений
         script.setAttribute('data-bot-access', 'true');
         // Добавляем скрипт в DOM
