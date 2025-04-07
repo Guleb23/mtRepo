c054ef736b0c198e52638f67f9d709ea9b6770ce
Guleb23 <112973753+Guleb23@users.noreply.github.com>
2025-04-07 21:16:12 +0300

    update

--- src/Components/CustomBtn.jsx
+++ src/Components/CustomBtn.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CustomBtn = ({ title, customStyles, onClick, icon }) => {
     return (
         <button onClick={onClick} className={` bg-[#E8EDF2]  gap-3 w-40 text-xs font-medium rounded-xl px-4 py-2 flex items-center justify-center w-cover hover:bg-[#b4b7bb] transition-all ${customStyles && customStyles}`}>
-            <p>{icon && icon}</p>
+            {icon ? <p>{icon && icon}</p> : ""}
             <p>{title}</p>
         </button>
     )
--- src/Components/Screens/LoginPage.jsx
+++ src/Components/Screens/LoginPage.jsx
@@ -5,56 +5,15 @@ import CustomBtn from '../CustomBtn';
 import axsios from '../../api/axsios';
 import useAuth from '../../Hooks/useAuth';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import TelegramLoginButton from '../TelegramLoginButton'; // Импортируем новый компонент
 
 const LOGIN_URL = '/login';
 
 const LoginPage = () => {
     const { setAuth } = useAuth();
 
-    const handleTelegramAuth = (user) => {
-        console.log('Telegram user data:', user);
-
-        alert(
-            `Вы вошли как ${user.first_name} ${user.last_name || ''} (ID: ${user.id}${user.username ? ', @' + user.username : ''
-            })`
-        );
-        sendDataToBackend(user);
-        // Здесь вы можете отправить данные пользователя на ваш сервер
-        // для проверки авторизации и создания сессии
-    };
     const [phone, setPhone] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
-
-
-    const sendDataToBackend = async (userData) => {
-        try {
-            const response = await axios.post("https://guleb23-webapplication2-a40c.twc1.net/auth/telegram", {
-                id: userData.id,
-                first_name: userData.first_name,
-                last_name: userData.last_name || '',
-                username: userData.username || '',
-                photo_url: userData.photo_url || '',
-                auth_date: userData.auth_date,
-                hash: userData.hash
-            },
-                {
-                    timeout: 10000, // Увеличиваем таймаут до 10 секунд
-                });
-
-            setAuth({
-                token: response.data.token,
-                id: response.data.id
-            });
-            localStorage.setItem("token", response.data.token);
-            localStorage.setItem("id", response.data.id);
-            navigate("/profile");
-        } catch (error) {
-            console.error("❌ Ошибка при отправке данных:", error);
-        }
-    };
 
     const handlePassword = (event) => {
         setPassword(event.target.value);
@@ -98,14 +57,6 @@ const LoginPage = () => {
                     title={`Войти`}
                     type="submit"
                 />
-
-                <TelegramLoginButton
-                    botName="mybotesgik_bot" // Ваш username бота
-                    buttonSize="large"    // large, medium или small
-                    onAuth={handleTelegramAuth}
-                    className="custom-telegram-button"
-                />
-
             </form>
 
         </div>
