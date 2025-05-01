35978e401da83c65c98478d3f6440e5410a1743a
Guleb23 <112973753+Guleb23@users.noreply.github.com>
2025-05-02 02:29:27 +0300

    фыв

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const AuthContext = createContext({});
 
@@ -7,26 +7,31 @@ export const AuthProvider = ({ children }) => {
     const [auth, setAuth] = useState({});
     const [loading, setLoading] = useState(true);
     const navigate = useNavigate();
+    const location = useLocation();
 
     useEffect(() => {
         const token = localStorage.getItem("token");
-        const id = localStorage.getItem("id");
-        const role = localStorage.getItem("role");
+        const id = Number(localStorage.getItem("id"));
+        const role = Number(localStorage.getItem("role"));
 
         if (token && id && role) {
-            // Восстанавливаем данные аутентификации
             setAuth({ token, id, role });
+
+            // Если роль менеджера и мы не на manager
+            if (role === 2 && location.pathname !== "/manager") {
+                navigate("/manager", { replace: true });
+            }
         } else {
-            setAuth({}); // Clear auth state if no tokens
+            setAuth({});
         }
-        setLoading(false); // Always set loading to false after check
-    }, [navigate]); // Add navigate to dependencies
+        setLoading(false);
+    }, [navigate]);
 
     return (
         <AuthContext.Provider value={{ auth, setAuth, loading }}>
             {children}
         </AuthContext.Provider>
-    )
-}
+    );
+};
 
 export default AuthContext;
